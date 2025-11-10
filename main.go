212ac65227c0e0ba06b6d205877a7c21fd22a2d3
jasminades <jasmina.destanovic1@gmail.com>
2025-11-10 13:59:28 +0100

    almost done

--- main.go
+++ main.go
@@ -1,16 +1,31 @@
 package main 
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+	"unicode/utf8"
+)
 
 func main() {
-	word := "golang"
+	word, err := getRandomWord("word_file.txt")
+
+	if err != nil {
+		fmt.Println("error occurred ", err)
+		return 
+	}
+
 	attempts := 6
-	currentWords := initalizeCurrentWordState(word)
+	currentWords := initializeCurrentWords(word)
 
 
 	scanner := bufio.NewScanner(os.Stdin)
 	guessedLetters := make(map[string]bool)
 	fmt.Println("Hangman Game :)")
 
-	for atempts > 0 {
+	for attempts > 0 {
 		currentState(currentWords, attempts)
 		userInput := getUserInput(scanner)
 
@@ -26,7 +41,58 @@ func main() {
 		guessedLetters[userInput] = true
 
 		correctGuess := updateGuessedLetters(word, currentWords, userInput)
+
+		if !correctGuess {
+			attempts--
+		}
+
+		displayHangman(6 - attempts)
+
+		if wordGuessed(currentWords, word) {
+			fmt.Println("correct !!!")
+			return
+		} 
+
+		if attempts == 0 {
+			fmt.Println(" game over, u lost :( , the correct word was ", word)
+			return
+		}
 	}
+}
+
+func getRandomWord(filename string) (string, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return "", err
+	}
+
+	words := strings.Split(string(data), "\n")
+	rand.Seed(time.Now().UnixNano())
+
+	return words[rand.Intn(len(words))], nil
+}
+
+
+func wordGuessed(guessed[] string, word string) bool {
+	return strings.Join(guessed, "") == word
+}
+
+func displayHangman(incorrectGuesses int){
+	if incorrectGuesses >= 0  && incorrectGuesses < len(hangman) {
+		fmt.Println(hangman[incorrectGuesses])
+	}
+}
+
+func updateGuessedLetters(word string, guessed []string, letter string) bool {
+	correctGuess := false 
+
+	for i, char := range word {
+		if string(char) == letter {
+			guessed[i] = letter
+			correctGuess = true
+		}
+	}
+	return correctGuess
 }
 
 func isValidInput(input string) bool {
