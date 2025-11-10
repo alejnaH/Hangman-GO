a8fbe2b04ec46572b150cbbfca3e9d233d6891f5
Alejna Hasanagic <alejna.hasanagic@stu.ibu.edu.ba>
2025-11-10 13:24:12 +0100

    Initialisation of words function

--- main.go
+++ main.go
@@ -8,3 +8,11 @@ func main() {
 	fmt.Println("Hangman Game :)")
 	displayCurrentState(currentWords, attempts)
 }
+
+func initializeCurrentWords(word string) []string {
+	currentWords := make([]string, len(word))
+	for i := range currentWords {
+		currentWords[i] = "_"
+	}
+	return currentWords
+}
